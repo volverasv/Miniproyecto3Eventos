372df2e7d7fcba1d2ecb3af296001e0e76aebcf3
AngieJoya99 <136751221+AngieJoya99@users.noreply.github.com>
2023-11-15 11:23:45 -0500

    Add files via upload

---
+++ Cliente/src/MainCliente.java
@@ -0,0 +1,10 @@
+public class MainCliente {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
