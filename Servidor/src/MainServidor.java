ccb3a99926a64fafc78ee40468864ea8ab33f074
AngieJoya99 <angie.joya@correounivalle.edu.co>
2023-11-17 12:47:12 -0500

    Angie - Declarar componentes de la GUI

--- Servidor/src/MainServidor.java
+++ Servidor/src/MainServidor.java
@@ -1,17 +1,16 @@
 import Modelo.Archivo;
 import Modelo.Examen;
 import Modelo.Pregunta;
+import Vista.GUIServidor;
 
 public class MainServidor {
 
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
-        Examen examen = new Examen("Examen 1", 2, 30, 0);
-        examen.addPregunta("Pregunta 1-;Opcion 1;Opcion 2;Opcion 3;Opcion 4;+Opcion 2+");
-        
-        
+    public static void main(String[] args) 
+    {
+        GUIServidor gui = new GUIServidor();
     }
     
 }
--- Servidor/src/Vista/GUIServidor.java
+++ Servidor/src/Vista/GUIServidor.java
@@ -1,18 +1,33 @@
 package Vista;
 
-import javax.swing.JFrame;
-import javax.swing.JPanel;
-import javax.swing.JTabbedPane;
+import javax.swing.*;
 
 public class GUIServidor extends JFrame 
 {
-    JTabbedPane pestanas;    
+    JTabbedPane pestanas;   
     JPanel pCrear, pVisualizar, pInformes, pHacer;
+    JScrollPane scVisualizar, scInforme;
 
+    JLabel lCrearNombre, lCrearArchivo, lCrearDuracion, 
+        lIniciarCantidad, lIniciarTiempo, lIniciarTiempoRestante, 
+        lIniciarTimer, lIniciarPreguntas, lIniciarRespondidas, 
+        lIniciarCliente1, lIniciarCliente2, lIniciarCliente3;
+
+    JButton bCrearCargar, bCrearCrear, bVerVer, bVerLimpiar, 
+        bInformeVer, bInformeLimpiar, bIniciarIniciar;
+
+    JTextField tfCrearNombre;
+    JTextArea taVisualizar, taInforme;
+    JComboBox cbVisualizar, cbInforme, cbIniciar;
+    JSpinner horas, minutos, segundos;
+
+    /**
+     * Constructor de la clase GUIServidor
+     */
     public GUIServidor()
     {
         setTitle("Servidor");
-        setSize(500, 560);
+        setSize(480, 560);
         crearGUI();
         setVisible(true);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
@@ -21,11 +36,14 @@ public class GUIServidor extends JFrame
 
     public void crearGUI()
     {
+        //Crear componentes
         pestanas = new JTabbedPane();
 
         pestanas.addTab("Crear examen",pCrear);
         pestanas.addTab("Visualizar examen",pVisualizar);
         pestanas.addTab("Ver informes",pInformes);
         pestanas.addTab("Realizar examen",pHacer);
+
+        add(pestanas);
     }
 }
